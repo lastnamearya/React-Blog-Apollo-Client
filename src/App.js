86bfab5a5f2bf34034b14152d71231917c0f8e91
lastnamearya <arya.jigyasu6815@gmail.com>
2019-01-01 02:20:20 +0530

    Apollo persistCache & InMemoryCache added

--- src/App.js
+++ src/App.js
@@ -1,46 +1,30 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
+
 import Posts from './Posts/Posts';
 import Post from './Posts/Post';
 import NewPost from './Posts/NewPost';
 import './App.css';
 
-const defaultState = {
-  isEditMode: false
-};
-
-const client = new ApolloClient({
-  uri: 'https://api-apeast.graphcms.com/v1/cjq3qnpg27ity01bn6r18efw1/master',
-  clientState: {
-    defaults: defaultState,
-    // It's a bug in Apollo as of now, resolvers isn't optional.
-    resolvers: {}
-  }
-});
-
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
-        <Router>
-          <div className="App">
-            <header className="App-header">
-              <Link to={'/'} style={{ textDecoration: 'none', color: 'white' }}>
-                <h1 className="App-title">GraphQL is Great</h1>
-              </Link>
-            </header>
-            <main>
-              <Switch>
-                <Route exact path="/" component={Posts} />
-                <Route exact path="/post/new" component={NewPost} />
-                <Route path="/post/:id" component={Post} />
-              </Switch>
-            </main>
-          </div>
-        </Router>
-      </ApolloProvider>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Link to={'/'} style={{ textDecoration: 'none', color: 'white' }}>
+              <h1 className="App-title">GraphQL is Great</h1>
+            </Link>
+          </header>
+          <main>
+            <Switch>
+              <Route exact path="/" component={Posts} />
+              <Route exact path="/post/new" component={NewPost} />
+              <Route path="/post/:id" component={Post} />
+            </Switch>
+          </main>
+        </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const defaultState = {
+  isEditMode: false
+};
+
+// In case of SSR, data needs to be supplimented into our Cache or this the place where we can bring our defaults in
+const cache = new InMemoryCache();
+
+// It makes sure that our cache first persists then it makes our Client to load.
+
+persistCache({
+  cache,
+  storage: window.localStorage
+}).then(() => {
+  const client = new ApolloClient({
+    // We need to add the cache, in our ApolloClient config
+    cache,
+    uri: 'https://api-apeast.graphcms.com/v1/cjq3qnpg27ity01bn6r18efw1/master',
+    clientState: {
+      defaults: defaultState,
+      // It's a bug in Apollo as of now, resolvers isn't optional.
+      resolvers: {}
+    }
+  });
+
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+  );
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
